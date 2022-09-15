f29e32b73a05a5b5915f8875528e0b0d01be3277
vikramkbgs@gmail.com <vikramkbgs@gmail.com>
2022-09-15 11:47:14 +0530

    creating models for TodoList

---
+++ models/todo.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+//1. creating schema
+
+const todoSchema = new mongoose.Schema({
+
+    task : {
+        type: String,
+        required: true
+    },
+     
+    catogery: {
+        type : String,
+        required : true
+    },
+
+    dueDate: {
+        type: Date,
+        required: true
+    },
+
+    priority : {
+        type: String,
+        required: true
+    },
+
+    taskColor: {
+       type: String,
+       required : true 
+    }
+});
+
+// 2. creating collection
+const TodoList = mongoose.model('TodoList', todoSchema);
+
+//3. exporting our collection
+module.exports = TodoList;
